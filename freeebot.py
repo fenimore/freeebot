998def2448cadbcbd41af2b8af7537b6dc80503d
polypmer <exorable.ludos@gmail.com>
2017-04-02 17:54:45 -0400

    fix directory mk

--- freeebot.py
+++ freeebot.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 """Twitter Bot for posting craigslist postings of Free Stuff
-    
+
 Currently set up for New York.
 
 Example usage:
     python tweetstuffs.py
-    
+
 Attributes:
     - NO_IMAGE -- link for when there is no image found
     - FILE -- path to tmp file
     - PATH -- current directory
     - C_KEY, C_SECRET, A_TOKEN, A_TOKEN_SECRET -- twitter api tokens
-    
+
 @author: Fenimore Love
 @license: MIT
 @date: 2015-2016
@@ -29,8 +29,8 @@ from datetime import datetime
 from time import gmtime, strftime, sleep
 import tweepy
 
-from freestuffs import stuff_scraper
-from stuff_scraper import StuffScraper
+#from freestuffs import stuff_scraper
+from freestuffs.stuff_scraper import StuffScraper
 from shortenurl import make_tiny
 from secrets import *
 
@@ -41,19 +41,19 @@ logfile_username = bot_username + ".log"
 
 PATH = os.getcwd()
 if not os.path.exists(PATH + '/tmp/'):
-    os.makedirs(directory)
+    os.makedirs(PATH + '/tmp/')
 if not os.path.exists(PATH + '/log/'):
-    os.makedirs(directory)
+    os.makedirs(PATH + '/log/')
 NO_IMAGE = 'http://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg'
 FILE = PATH + '/tmp/tmp-filename.jpg'
 
 
 def create_tweet(stuff):
     """Create string for tweet with stuff."""
-    post = {"title": stuff['title'], 
-            "loc" : stuff['location'], 
-            "url" : make_tiny(stuff['url'])} 
-    _text = post["loc"] + "\n" + post["title"] +" " + post["url"]          
+    post = {"title": stuff['title'],
+            "loc" : stuff['location'],
+            "url" : make_tiny(stuff['url'])}
+    _text = post["loc"] + "\n" + post["title"] +" " + post["url"]
     _text = check_length(_text, post)
     return _text
 
@@ -69,7 +69,7 @@ def tweet(new_stuffs_set):
         for stuff in stuffs:
             tweet = create_tweet(stuff)
             if str(stuff['image']) == NO_IMAGE:
-                isImage = False 
+                isImage = False
             else:
                 isImage = True
                 urllib.request.urlretrieve(stuff['image'], FILE)
@@ -77,7 +77,7 @@ def tweet(new_stuffs_set):
                 if isImage:
                     log("\n\n Posting with Media \n " + tweet + "\n ----\n")
                     api.update_with_media(FILE, status=tweet)
-                else: 
+                else:
                     api.update_status(tweet)
                     log("\n\n Posting without media\n "
                          + tweet + "\n ----\n")
@@ -89,15 +89,15 @@ def tweet(new_stuffs_set):
 
 def check_length(tweet, post):
     """Check if tweet is proper length.
-    
+
     TODO: Twitter has recently changed how it counts characters.
     """
     if len(tweet) < 145: # tweet is good
         return tweet
     else:
         log("Tweet too long")
-        tweet = post["loc"] + "\n" + post["title"] + " " + post["url"] 
-        if len(tweet) > 144: # tweet is still not good 
+        tweet = post["loc"] + "\n" + post["title"] + " " + post["url"]
+        if len(tweet) > 144: # tweet is still not good
             tweet = post["title"] + " " + post["url"]
             return tweet
         return tweet
@@ -114,22 +114,22 @@ def log(message):
 
 if __name__ == "__main__":
     """Tweet newly posted Free stuff objects.
-    
+
     Using sets of the tupled-sorted-dict-stuffs to compare
-    old scrapes and new. No need calling for precise, as 
+    old scrapes and new. No need calling for precise, as
     twitter doesn't need the coordinates. If the set has 15
     items, doesn't post, in order to stop flooding twitter
     on start up.
     """
     process_log = open(logfile_username,'a+')
     _location = 'newyork' # TODO: Change to brooklyn?
-    stale_set = set() # the B set is what has already been 
+    stale_set = set() # the B set is what has already been
     log("\n\nInitiating\n\n")
     while True:
         stuffs = [] # a list of dicts
         for stuff in StuffScraper(_location, 15).stuffs: # convert stuff
             stuff_dict = {'title':stuff.thing,      # object into dict
-                          'location':stuff.location, 
+                          'location':stuff.location,
                           'url':stuff.url, 'image':stuff.image}
             stuffs.append(stuff_dict)
         fresh_set = set() # A set, Fresh out the oven
@@ -140,7 +140,7 @@ if __name__ == "__main__":
         ready_set = fresh_set - stale_set # Get the difference
         stale_set = fresh_set
         if len(list(ready_set)) is not 15:
-            tweet(ready_set) 
+            tweet(ready_set)
         log("\n    New Stuffs : " + str(len(list(ready_set)))+
             "\n Todays Stuffs : "+ str(len(list(stale_set)))+
             "\n\n Sleep Now (-_-)Zzz... \n")
